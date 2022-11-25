fce34d6687e68410a21cd677c7c7ae86af8b00a8
Haeun Noh <00blowup@gmail.com>
2022-11-26 02:00:29 +0900

    Initial commit

---
+++ src/main/java/hello/clothingdemo/ClothingDemoApplication.java
@@ -0,0 +1,13 @@
+package hello.clothingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/clothingdemo/ClothingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.clothingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
